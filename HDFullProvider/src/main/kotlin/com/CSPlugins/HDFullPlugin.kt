f44930a8da18262fbe2927bf45858bccd55a9276
Mioki <22417711+okibcn@users.noreply.github.com>
2026-02-02 15:59:24 -0800

    fix warnings

--- HDFullProvider/src/main/kotlin/com/CSPlugins/HDFullPlugin.kt
+++ HDFullProvider/src/main/kotlin/com/CSPlugins/HDFullPlugin.kt
@@ -9,7 +9,5 @@ class HDFullPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(HDFull())
-        registerExtractorAPI(VidmolyOki())
-
     }
 }
