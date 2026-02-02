5a3851bf4518983f23e506fa86390d3c6f3c8266
Mioki <22417711+okibcn@users.noreply.github.com>
2026-02-02 14:42:17 -0800

    async calls

--- LATAnimeProvider/src/main/kotlin/com/LATAnime/LATAnimeProvider.kt
+++ LATAnimeProvider/src/main/kotlin/com/LATAnime/LATAnimeProvider.kt
@@ -99,7 +99,6 @@ class LATAnimeProvider : MainAPI() {
         }
     }
 
-
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -107,8 +106,9 @@ class LATAnimeProvider : MainAPI() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         val document = app.get(data).documentLarge
-        document.select("#play-video a").map {
-            val href = base64Decode(it.attr("data-player")).substringAfter("=")
+        // concurrent loadExtractor calls
+        document.select("#play-video a").amap { element ->
+            val href = base64Decode(element.attr("data-player")).substringAfter("=")
             loadExtractor(
                 href,
                 "",
@@ -119,103 +119,31 @@ class LATAnimeProvider : MainAPI() {
         return true
     }
 
+    // override suspend fun loadLinks(
+    //     data: String,
+    //     isCasting: Boolean,
+    //     subtitleCallback: (SubtitleFile) -> Unit,
+    //     callback: (ExtractorLink) -> Unit
+    // ): Boolean {
+    //     val document = app.get(data).documentLarge
+    //     document.select("#play-video a").map {
+    //         val href = base64Decode(it.attr("data-player")).substringAfter("=")
+    //         loadExtractor(
+    //             href,
+    //             "",
+    //             subtitleCallback,
+    //             callback
+    //         )
+    //     }
+    //     return true
+    // }
+
     private fun Element.getImageAttr(): String? {
         return this.attr("data-src")
             .takeIf { it.isNotBlank() && it.startsWith("http") }
             ?: this.attr("src").takeIf { it.isNotBlank() && it.startsWith("http") }
     }
 }
- 
- 
- 
-    // override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-    //     val urls = listOf(
-    //             Pair("$mainUrl/emision", "Novedades"),
-    //             Pair("$mainUrl/buscar?q=Castellano", "Anime Castellano"),
-    //             Pair("$mainUrl/buscar?q=Latino", "Anime Latino"),
-    //             Pair("$mainUrl/animes?categoria=Película+Castellano", "Peliculas Castellano"),
-    //             Pair("$mainUrl/animes?categoria=Película+Latino", "Peliculas Latino"),
-    //             Pair("$mainUrl/animes?categoria=castellano", "Series Castellano"),
-    //             Pair("$mainUrl/animes?categoria=latino", "Series Latino"),
-    //             Pair("$mainUrl/animes", "Todo el Anime"),
-    //             Pair("$mainUrl/animes?categoria=ova", "OVA"),
-    //             Pair("$mainUrl/animes?categoria=sin-censura", "Sin Censura"),
-    //     )
-
-    //     val items = ArrayList<HomePageList>()
-    //     val isHorizontal = true
-
-    //     urls.amap { (url, name) ->
-    //         val home = wGet(url).document.select("html body div.container div.row div.col-md-4.col-lg-3.col-xl-2.col-6.my-3").map {
-    //             val title = it.selectFirst("div.col-md-4.col-lg-3.col-xl-2.col-6.my-3 a div.series div.seriedetails h3.my-1")!!.text()
-    //             val imgElement = it.selectFirst("div.col-md-4.col-lg-3.col-xl-2.col-6.my-3 a div.series div.serieimg.shadown img.img-fluid2.shadow-sm")
-    //             val poster =
-    //                     if(imgElement?.attr("data-src")?.isEmpty() == false)
-    //                         imgElement.attr("data-src") else
-    //                             imgElement?.attr("src") ?: ""
-
-    //             newAnimeSearchResponse(title, fixUrl(it.selectFirst("a")!!.attr("href"))) {
-    //                 this.posterUrl = fixUrl(poster)
-    //                 addDubStatus(getDubStatus(title))
-    //                 this.posterHeaders = if (poster.contains(mainUrl)) cloudflareKiller.getCookieHeaders(mainUrl).toMap() else emptyMap<String, String>()
-    //             }
-    //         }
-
-    //         items.add(HomePageList(name, home))
-    //     }
-    //     if (items.size <= 0) throw ErrorLoadingException()
-    //     return newHomePageResponse(items)
-    // }
-
-//     override suspend fun search(query: String): List<SearchResponse> {
-//         return wGet("$mainUrl/buscar?q=$query").document.select("html body div.container div.row div.col-md-4.col-lg-3.col-xl-2.col-6.my-3").map {
-//             val title = it.selectFirst("a div.series div.seriedetails h3.my-1")!!.text()
-//             val href = fixUrl(it.selectFirst("a")!!.attr("href"))
-//             val image = it.selectFirst("a div.series div.serieimg.shadown img.img-fluid2.shadow-sm")!!.attr("src")
-//             newAnimeSearchResponse(title, href, TvType.Anime){
-//                 this.posterUrl = fixUrl(image)
-//                 this.dubStatus = if (title.contains("Latino") || title.contains("Castellano")) EnumSet.of(
-//                             DubStatus.Dubbed
-//                     ) else EnumSet.of(DubStatus.Subbed)
-// //                this.posterHeaders = if (image.contains(mainUrl)) cloudflareKiller.getCookieHeaders(mainUrl).toMap() else emptyMap<String, String>()
-//             }
-//         }
-//     }
-
-//     override suspend fun load(url: String): LoadResponse? {
-//         val doc = wGet(url).document
-//         val poster = doc.selectFirst("div.col-lg-3.col-md-4 div.series2 div.serieimgficha img.img-fluid2")!!.attr("src")
-//         val backimage = doc.selectFirst("div.col-lg-3.col-md-4 div.series2 div.serieimgficha img.img-fluid2")!!.attr("src")
-//         val title = doc.selectFirst("div.col-lg-9.col-md-8 h2")!!.text()
-//         val type = doc.selectFirst("div.chapterdetls2")?.text() ?: ""
-//         val description = doc.selectFirst("div.col-lg-9.col-md-8 p.my-2.opacity-75")!!.text()
-//         val genres = doc.select("div.col-lg-9.col-md-8 a div.btn").map { it.text() }
-//         val status = when (doc.selectFirst("div.col-lg-3.col-md-4 div.series2 div.serieimgficha div.my-2")?.text()) {
-//             "Estreno" -> ShowStatus.Ongoing
-//             "Finalizado" -> ShowStatus.Completed
-//             else -> null
-//         }
-//         // Log.d("depurando", "load: title $title")
-//         val episodes = doc.select("div.container div.row div.row div a").map {
-//             val name = it.selectFirst("div.cap-layout")!!.text()
-//             val link = it!!.attr("href")
-//             // Log.d("depurando", "load: link $link")
-//             val epThumb = it.selectFirst(".animeimghv")?.attr("data-src")
-//                     ?: it.selectFirst("div.animeimgdiv img.animeimghv")?.attr("src")
-//             newEpisode(link){
-//                 this.name = name
-//             }
-//         }
-//         return newAnimeLoadResponse(title, url, getType(title)) {
-//             posterUrl = poster
-//             backgroundPosterUrl = backimage
-//             addEpisodes(DubStatus.Subbed, episodes)
-//             showStatus = status
-//             plot = description
-//             tags = genres
-//             posterHeaders = if (poster.contains(mainUrl)) cloudflareKiller.getCookieHeaders(mainUrl).toMap() else emptyMap<String, String>()
-//         }
-//     }
 
 //     suspend fun customLoadExtractor(
 //         url: String,
@@ -238,22 +166,3 @@ class LATAnimeProvider : MainAPI() {
 //             .replaceFirst("https://do7go.com", "https://dood.la")
 //             , referer, subtitleCallback, callback)
 //     }
-
-//     override suspend fun loadLinks(
-//             data: String,
-//             isCasting: Boolean,
-//             subtitleCallback: (SubtitleFile) -> Unit,
-//             callback: (ExtractorLink) -> Unit
-//     ): Boolean {
-//         wGet(data).document.select("li#play-video").amap {
-//             val encodedurl = it.select("a").attr("data-player")
-//             val urlDecoded = base64Decode(encodedurl)
-//             // Log.d("depurando", "urlDecoded: $urlDecoded")
-//             val url = (urlDecoded)
-//                 .replace("https://monoschinos2.com/reproductor?url=", "")
-//                 .replace("https://mojon.latanime.org/aqua/fn?url=", "")
-//             customLoadExtractor(url, mainUrl, subtitleCallback, callback)
-//         }
-//         return true
-//     }
-// }
