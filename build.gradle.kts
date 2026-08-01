0f038765d49275b8c9b88f63bfa07f0c810a948d
Mioki <22417711+okibcn@users.noreply.github.com>
2026-07-31 20:52:04 -0700

    compileOnly to implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,11 +72,10 @@ subprojects {
     }
 
     dependencies {
-        val compileOnly by configurations
         val implementation by configurations
 
         // Stubs for all Cloudstream classes from new repository
-        compileOnly("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
