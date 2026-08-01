af4a324bcd862fc118e079fde97ea8f6a4c9206c
Mioki <22417711+okibcn@users.noreply.github.com>
2026-07-31 20:47:30 -0700

    added library delimiter

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes from new repository
-        compileOnly("com.github.recloudstream:cloudstream:-SNAPSHOT")
+        compileOnly("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
