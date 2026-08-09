ad48de1fa920b293cfc9d2d67dcab62b91cd3499
Mioki <22417711+okibcn@users.noreply.github.com>
2026-08-08 20:15:44 -0700

    fix blugin definigion

--- HDFullProvider/src/main/kotlin/com/CSPlugins/HDFullPlugin.kt
+++ HDFullProvider/src/main/kotlin/com/CSPlugins/HDFullPlugin.kt
@@ -1,14 +1,29 @@
 package com.CSPlugins
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import android.content.Context
 
 @CloudstreamPlugin
-class HDFullPlugin: Plugin() {
-    override fun load(context: Context) {
+class HDFullPlugin: BasePlugin() {
+    override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(HDFull())
         registerExtractorAPI(MyVidmoly())
     }
 }
+
+// package com.CSPlugins
+
+// import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+// import com.lagradost.cloudstream3.plugins.Plugin
+// import android.content.Context
+
+// @CloudstreamPlugin
+// class HDFullPlugin: Plugin() {
+//     override fun load(context: Context) {
+//         // All providers should be added in this manner. Please don't edit the providers list directly.
+//         registerMainAPI(HDFull())
+//         registerExtractorAPI(MyVidmoly())
+//     }
+// }
